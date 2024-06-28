1e172c2496f3edbd7f8ee958f2075f19bfb7e2df
Abiud-lang <abiud.kiprotich@student.moringaschool.com>
2024-06-28 18:28:11 +0300

    1st class functions

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solution in this file!
+
+const returnFirstTwoDrivers = function() {
+  return (['Antonia', 'Nuru',]);
+};
+
+ const returnLastTwoDrivers = function() {
+    return ( ['Amari', 'Mo'] )
+ };
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+const createFareMultiplier = function () {
+  return function () {
+    return 5 * 5;
+  };
+
+}
+
+const fareDoubler = function () {
+   return 10 * 2 
+}
+
+ const fareTripler = function () {
+   return 12 * 3 
+ } 
+ 
+ const selectDifferentDrivers = function (returnLastTwoDrivers, returnFirstTwoDrivers) {
+    return returnFirstTwoDrivers();
+  };
+  
+ 
